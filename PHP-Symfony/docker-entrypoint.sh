a19947f4a4b86048f523980f321323bb56076b46
Nazim Lachter <lachtater@gmail.com>
2017-09-15 17:03:39 +0200

    Disabling assetic and schema update

--- PHP-Symfony/docker-entrypoint.sh
+++ PHP-Symfony/docker-entrypoint.sh
@@ -13,17 +13,17 @@ if [ -z "$WORKDIR" ]
 	
 	if [[ -d "var/cache" ]]; then rm -rf var/cache/*; fi
 
-    echo "# Develoment assets (No Interaction, Full verbose)"
-	gosu www-data php bin/console assetic:dump -n -vvv
+    #echo "# Develoment assets (No Interaction, Full verbose)"
+	#gosu www-data php bin/console assetic:dump -n -vvv
 
-    echo "# Production assets (No Interaction, No debug mode, Production, Full verbose)"
-	gosu www-data php bin/console assetic:dump -n --no-debug -e prod -vvv
+    #echo "# Production assets (No Interaction, No debug mode, Production, Full verbose)"
+	#gosu www-data php bin/console assetic:dump -n --no-debug -e prod -vvv
 
     echo "# Parameters and Bootsrap"
 	gosu www-data composer run-script symfony-part1 --no-interaction --verbose
 
-    echo "# Database update"
-	gosu www-data php bin/console doctrine:schema:update --force
+    #echo "# Database update"
+	#gosu www-data php bin/console doctrine:schema:update --force
 
     echo "# Cache, assets, requirements, deploying"
 	gosu www-data composer run-script symfony-part2 --no-interaction --verbose
